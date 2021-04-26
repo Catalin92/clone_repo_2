8f3bdb5d30a139db72db8c768b945928e04bdd03
catalin <cretoiu.catalin20@gmail.com>
2021-04-26 23:16:08 +0300

    Fixed some issues

--- Webcrawler.py
+++ Webcrawler.py
@@ -27,7 +27,8 @@ def get_tuple_country_link(oject):
     for i in list1:
         j = re.findall('title="(.+?)".+href="(.+)"', str(i), flags=re.I)
         countries.append(j[0])
-
+    print(countries)
+    print(len(countries))
     return countries
 
 def advanced_search(country, page_number=1):
@@ -42,16 +43,19 @@ def iterate_over_pages(country, page_number):
 
 def get_countryString_in_url(url):
     a = url.split(".")[0]
+    a = a.replace("/catalogue/","")
     return a[:-2]
 
 def get_numer_of_coins(url, country):
     more_results_on_page = "?q=100"
-    # print("url is ", url + country + more_results_on_page)
+    print("url is ", url + country + more_results_on_page)
     soup2 = scan_url_return_soup(url + country + more_results_on_page)
     catalogue = soup2.findAll("div", {"class": "catalogue_navigation"})
     if not catalogue:
         return "0"
-    j = re.findall('(\d+)\s+(?:coins|coin)\s+found', str(catalogue[0]), flags=re.I)
+    j = re.findall('(\d+)\s+(?:results|result)\s+found', str(catalogue[0]), flags=re.I)
+    print("Number of coins is: ")
+    print(catalogue[0])
     return j[0]
 
 def calc_pages(coins):
@@ -70,7 +74,9 @@ def calc_pages(coins):
 def fooo(country):
     name = country[0]
     country_url = country[1]
+    print("Name of country: " + name + "; URL: " + country_url)
     country_part = get_countryString_in_url(country_url)
+    print(advanced_search(country_part))
     nr_of_coins = get_numer_of_coins(url, advanced_search(country_part))
     print(country_part + " : " + nr_of_coins)
     if int(nr_of_coins) > 0:
@@ -82,21 +88,21 @@ def fooo(country):
     # url_coin = iterate_over_pages(country=country_part, page_number=1)
     # print(" link " + url_coin)
     print("pages : " + str(nr_pages))
+
     for i in range(1, nr_pages+1):
         url_coin = iterate_over_pages(country=country_part, page_number=i)
+        print("##########")
         print(url_coin)
 
         scaned = scan_url_return_soup(url_coin)
 
         all_coins = scaned.findAll("div", {"id": "resultats_recherche"})[0]
         list1 = all_coins.findAll("div", {"class": "resultat_recherche"})
-        # print(list1)
 
         loo = []
         for i in list1[:-1]:
             j = re.findall('href="(.+\.html)"', str(i), flags=re.I)
             loo.append(j[0])
-        print(loo)
 
         p = ThreadPool(5)
         # aa = p.map(extract_info_and_create_coin, loo[0:1])
@@ -107,54 +113,6 @@ def fooo(country):
 
 
 
-        # print(len(aa))
-        # url1 = "https://en.numista.com/catalogue/pieces30261.html"
-        # extract_info_and_create_coin("pieces30261.html")# p = ThreadPool(5)
-        #     # aa = p.map(extract_info_and_create_coin, loo)
-        #     # print(len(aa))
-        #     # url1 = "https://en.numista.com/catalogue/pieces30261.html"
-        #     # extract_info_and_create_coin("pieces30261.html")
-
-
-# x = time.time()
-# for country in countries:
-#     name = country[0]
-#     country_url = country[1]
-#     country_part = get_countryString_in_url(country_url)
-#     # print(country_part)
-#     nr_of_coins = get_numer_of_coins(url, advanced_search(country_part))
-#     print(country_part+ " : "+ nr_of_coins  )
-# y= time.time()
-# print(y-x)
-
-
-
-
-
-
-##################################################
-#Files.create_dir("countries")
-
-# url2 = 'https://en.numista.com/catalogue/'
-#
-# first_country_tuple = countries[0]
-# urll = first_country_tuple[1]
-#
-# print(urll)
-# countr_string = get_countryString_in_url(urll)
-#
-# nr_of_coins = get_numer_of_coins(url2, advanced_search(countr_string))
-# print(nr_of_coins)
-# nr_of_pages = calc_pages(int(nr_of_coins))
-#
-# print(nr_of_pages)
-
-
-# for i in range(1, nr_of_pages+1):
-#     print(iterate_over_pages(countr_string, i))
-# country, years, value, metal, coin_type, weight, diameter, shape, rarity, demonetized, link,references)
-#country, years, value, metal, coin_type, weight, diameter, shape, rarity = '-', demonetized='-'
-
 def checking_regex(result):
     # print(result)
     if len(result) == 0:
@@ -163,10 +121,15 @@ def checking_regex(result):
         return result[0]
 
 def create_coin(string, country, link, rarity):
-    reg = re.findall('<th>(?:Years|Year)</th>\s+<td>(.+)</td>', string, flags=re.I)
+    reg = re.findall('<th>(?:Years|Year)<\/th>\s+<td>(.+|\n.+)<\/td>', string, flags=re.I)
     years = checking_regex(reg)
+    years = years.replace(" ", "")
+    years = years.replace("\n", "")
+    print("*****************************")
+    print(years)
+    # reg = re.findall('<th>Value<\/th>\s+<td[\s\S]+?>([\s\S]+?)(?:\(|<)', string, flags=re.I)
+    reg = re.findall('<th>Value<\/th>\s+<td>([\s\S]+?)<', string, flags=re.I)
 
-    reg = re.findall('<th>Value</th>\s+<td[\s\S]+?>([\s\S]+?)(?:\(|<)', string, flags=re.I)
     value = checking_regex(reg).replace("\n", "")
     value = " ".join(value.split()).replace("/", "-")
     if '<' in value or '>' in value:
@@ -174,34 +137,35 @@ def create_coin(string, country, link, rarity):
         value = value.replace('<', '')
     if '"' in value:
         value = value.replace('"','')
-
-    reg = re.findall('<th>Metal</th>\s+<td>(.+)</td>', string, flags=re.I)
+    value = value.replace("&amp;nbspAFA", "")
+    print(value)
+    reg = re.findall('<th>Composition</th>\s+<td>(.+)</td>', string, flags=re.I)
     metal = checking_regex(reg)
-
+    print(metal)
     reg = re.findall('<th>Type</th>\s+<td>(.+)</td>', string, flags=re.I)
     coin_type = checking_regex(reg)
-
+    print(coin_type)
     reg = re.findall('<th>Weight</th>\s+<td>(.+)</td>', string, flags=re.I)
     weight = checking_regex(reg).replace(r"x\a0", "")
-
+    print(weight)
     reg = re.findall('<th>Diameter</th>\s+<td>(.+)</td>', string, flags=re.I)
     diameter = checking_regex(reg).replace(r"x\a0", "")
-
+    print(diameter)
     reg = re.findall('<th>Shape</th>\s+<td>(.+)</td>', string, flags=re.I)
     shape = checking_regex(reg)
-
+    print(shape)
     reg = re.findall('<th>References</th>[\s\S]+?KM</abbr>([\s\S]+?)<', string, flags=re.I|re.M)
-    references = checking_regex(reg).replace(",", "").replace("?", "").strip()
+    references = checking_regex(reg).replace(",", "").replace("#", "").replace("?", "").strip()
 
     reg = re.findall('<th>Demonetized</th>\s+<td>(.+)</td>', string, flags=re.I)
     demonetized = checking_regex(reg)
-
+    print(demonetized)
     coin = Coin(country, years, value, metal, coin_type, weight, diameter, shape, rarity, demonetized, link, references)
     return coin
 
 
 def extract_info_and_create_coin(soup_str, country):
-    url1 = "https://en.numista.com/catalogue/" + soup_str
+    url1 = "https://en.numista.com" + soup_str
     soup = scan_url_return_soup(url1)
 
     reg = re.findall("numista\s+rarity\s+index:\s+<\w+>(\d+)", str(soup), flags=re.I | re.M)
@@ -211,10 +175,13 @@ def extract_info_and_create_coin(soup_str, country):
     object = caract.findChildren("tr")
     string = str(object)
     unique_nr = soup_str[:-5]
+    unique_nr = unique_nr.replace("/catalogue/", "")
 
     coin = create_coin(string, country, url1, rarity)
 
     coin_folder = 'countries\\' + country + f'\\{coin.value}_{coin.years}_{coin.references}__{unique_nr}'
+
+    print(coin_folder)
     create_dir(coin_folder)
     write_details(coin_folder + "\\Details.txt", coin)
 
@@ -229,7 +196,10 @@ def extract_info_and_create_coin(soup_str, country):
         return
 
     photo1 = photo.findAll("a", {"data-fluidbox": ""})
-    print(coin.country, coin.value, coin.years, coin.rarity, coin.references )
+    print(coin.country, coin.value, coin.years, coin.rarity, coin.references)
+
+    if(len(photo1) == 0):
+        return
 
     if 'obverse' in str(photo1[0]):
         avers = re.findall('src="(.+?)"', str(photo1[0]), flags=re.I)
@@ -242,52 +212,18 @@ def extract_info_and_create_coin(soup_str, country):
 
 
 
-
-    # return c
-
-
-# x = time.time()
-# print(loo)
-# for i in loo:
-#     url1 = "https://en.numista.com/catalogue/" + i
-#     soupp = scan_url_return_soup(url1)
-#
-#     extract_info_and_create_coin(soupp)
-#     print("##########")
-# if __name__ == '__main__':
-    # p = ThreadPool(5)
-    # aa = p.map(extract_info_and_create_coin, loo)
-    # print(len(aa))
-    # url1 = "https://en.numista.com/catalogue/pieces30261.html"
-    # extract_info_and_create_coin("pieces30261.html")
-
-# soupp = scan_url_return_soup(url1)
-#
-# extract_info_and_create_coin(soupp)
-# y= time.time()
-#
-# print(y-x)
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     print("######################")
     url = "https://en.numista.com/catalogue/"
     soup_obj = scan_url_return_soup(url + "pays.php")
     countries = get_tuple_country_link(soup_obj)
-
+    print(len(countries))
     x = time.time()
     p = ThreadPool(5)
-    # aa = p.map(fooo, countries[:-2])
-    aa = p.map(fooo, countries[240:-2])
+    # aa = p.map(fooo, countries[0:4])
+    # fooo(countries[114])
+    # fooo(countries[113])
+    aa = p.map(fooo, countries[240:245])
+    # aa = p.map(fooo, countries[240:-2])
     y= time.time()
     print(y-x)
